dfec4edd4ae5ee63b686f06fd5e211c3048801a3
JvanZyl1 <jonathan.vanzyl2001@hotmail.com>
2025-05-15 15:20:47 +0200

    last run, (1e-5 actor lr), upping to 1e-3 for the new run

--- configs/agent_config.py
+++ configs/agent_config.py
@@ -239,7 +239,7 @@ config_landing_burn = {
         'trajectory_length': 10,
         'batch_size': 512,
         'critic_learning_rate': 1e-3,
-        'actor_learning_rate': 1e-5,
+        'actor_learning_rate': 1e-3,
         'temperature_learning_rate': 6e-3,
         'critic_grad_max_norm': 0.2,
         'actor_grad_max_norm': 0.2,
